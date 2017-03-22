2defdc55f6ef0f242d6408c69e7fbdf508b78ebd
AChepurnoi <sasha.chepurnoii@gmail.com>
2017-03-23 01:14:32 +0200

    Init commit

---
+++ web/src/js/client.js
@@ -0,0 +1,6 @@
+var jquery = require('../assets/javascripts/jquery-3.2.0.js');
+window.jQuery = jquery;
+window.$ = jquery;
+require('../assets/javascripts/bootstrap.js');
+
+document.write("Hello world!");
