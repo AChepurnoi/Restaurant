dbdad41481288e978416c5c7e1ce3740876cecda
AChepurnoi <sasha.chepurnoii@gmail.com>
2017-05-29 23:17:47 +0300

    Added pagination for dish list

--- web/src/js/components/menu/DishList.js
+++ web/src/js/components/menu/DishList.js
@@ -7,11 +7,16 @@ export default class DishList extends React.Component{
 
     constructor(props) {
         super(props);
+        this.state = {
+            limit: 3,
+            offset: 0
+        }
       
     }
 
     
 	render(){
+
 
         let addBtn;
         let delBtn = (id) => {};
@@ -29,6 +34,22 @@ export default class DishList extends React.Component{
 
 
         }
+
+        let nextPage =  <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next"
+                               onClick={() => this.setState({offset: this.state.offset + 3})}>
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>;
+
+        let prevPage =  <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous"
+                               onClick={() => this.setState({offset: this.state.offset - 3})}>
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>;
 
 
 		return <div class="row"> 
@@ -66,8 +87,15 @@ export default class DishList extends React.Component{
                                 <div class="panel-content">{item.description}</div>
                               </div>
                             </div>
-                           </div>))}
+                           </div>)).slice(this.state.offset, this.state.offset + this.state.limit)}
                 </div>
+                <nav aria-label="Page navigation example" className="text-center">
+                    <ul class="pagination">
+                        {this.state.offset > 0 && prevPage}
+                        <li class="page-item"><a class="page-link" href="#">{(this.state.offset / 3) + 1}</a></li>
+                        {this.state.offset + 3 < this.props.items.length && nextPage}
+                    </ul>
+                </nav>
               </div>
                     
 	}
